c41281f114bda32ca9225e5e6cbe64cb875abbff
herliana oktaviani <71244505+Herlianaoktv@users.noreply.github.com>
2023-05-23 15:45:03 +0700

    Delete app directory

--- app/src/main/java/com/example/discountcalculate/model/HasilDiskon.kt
+++
@@ -1,6 +0,0 @@
-package com.example.discountcalculate.model
-
-data class HasilDiskon(
-    val hitungDiskon1 : Float,
-    val totalDiskon: Float
-)
--- app/src/main/java/com/example/discountcalculate/model/HitungDiskon.kt
+++
@@ -1,9 +0,0 @@
-package com.example.discountcalculate.model
-
-import com.example.discountcalculate.db.DiskonEntity
-
-fun DiskonEntity.hitungDiskon():HasilDiskon{
-    val hitungDiskon1 = harga * (diskon/100)
-    val totalDiskon = harga - hitungDiskon1
-    return HasilDiskon(hitungDiskon1, totalDiskon)
-}
